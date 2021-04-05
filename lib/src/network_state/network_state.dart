4a1df6f2681f85b2443d62c32a77cf33f03f1f5c
Ashish <ashishrawat2911@gmail.com>
2021-04-05 10:35:13 +0530

    updated result state

--- lib/src/network_state/network_state.dart
+++ lib/src/network_state/network_state.dart
@@ -1,4 +1,4 @@
 export 'package:vento/src/network_state/error/result_error.dart';
 export 'package:vento/src/network_state/result/result.dart';
 export 'package:vento/src/network_state/result_state/result_state.dart';
-export 'package:vento/src/network_state/result_state/result_api_builder.dart';
+export 'package:vento/src/network_state/result_state/result_builder.dart';
